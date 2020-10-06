0581cb051479a1579b05ec31fb21d0e92e56591c
Ryan Shin <ryanminjaeshin@gmail.com>
2020-10-05 18:57:08 -0700

    Update proxy

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const proxy = createProxyMiddleware({
+  target: 'http://localhost:8080',
+  changeOrigin: true, // for vhosted sites, changes host header to target's host
+  logLevel: 'debug',
+});
 
 const app = express();
 
@@ -9,6 +16,7 @@ const PORT = 9999;
 // const bodyParser = require('body-parser');
 
 app.use(cors());
+app.use('/products/', proxy);
 // app.use('/', router)
 app.use(express.static(path.join(__dirname, '../client')));
 
