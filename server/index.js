146bffc174ab9599db32e037f3eab71eeda8481d
Ryan Shin <ryanminjaeshin@gmail.com>
2020-10-11 17:38:24 -0700

    Final proxy version

--- server/index.js
+++ server/index.js
@@ -9,30 +9,6 @@ const port = 9999;
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(cors());
 
-// app.get('/products/:src/:id', (req, res) => {
-//   const { src, id } = req.params;
-//   let path;
-
-//   if(src === 'featureExplorer') {
-//     path = 'http://localhost:3000';
-//   }
-//   else if(src === 'relatedProducts') {
-//     path = 'http://localhost:3001';
-//   }
-//   else if(src === 'productInfo') {
-//     path = 'http://localhost:3002';
-//   }
-
-//   axios.get(`${path}/products/${id}`)
-//   .then((response) => {
-//     res.status(200).send(response.data);
-//   })
-//   .catch((error) => {
-//     res.status(400).send(error);
-//   })
-// });
-
-
 const Proxy = (targetUrl) => (req, res) => {
   axios.get(targetUrl + req.originalUrl)
     .then((response) => {
@@ -44,9 +20,10 @@ const Proxy = (targetUrl) => (req, res) => {
 };
 const proxyFeatureExplorer = Proxy('http://3.101.127.251');
 const proxyRelatedProducts = Proxy('http://18.144.2.219');
-const proxyProductInfo = Proxy('http://3.138.107.143');
+const proxyProductInfo = Proxy('http://3.129.22.244');
 const proxyQuestions = Proxy('http://54.183.5.42');
 const proxyReviews = Proxy('http://3.131.135.129');
+
 app.use('/products/featureExplorer/:id', proxyFeatureExplorer);
 app.use('/products/relatedProducts/:id', proxyRelatedProducts);
 app.use('/products/productInfo/:id', proxyProductInfo);
